a6af80b4cdea258c4c38d94974952d19cdf51d64
Álvaro Hernández <aht@ongres.com>
2025-05-15 21:49:30 -0400

    chore(monobot): Remove unneeded pauseForDebugging config property
    This functionality is already provided with -Dsuspend

--- monobot/src/main/java/dev/monogres/monobot/main/Main.java
+++ monobot/src/main/java/dev/monogres/monobot/main/Main.java
@@ -5,7 +5,6 @@ import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import jakarta.inject.Inject;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @QuarkusMain
 public class Main {
@@ -16,14 +15,8 @@ public class Main {
   public static class MyApp implements QuarkusApplication {
     @Inject Scan scan;
 
-    @ConfigProperty(name = "pauseForDebugging")
-    long pauseForDebugging;
-
     @Override
     public int run(String... args) throws Exception {
-      // This is useful for debugging if you want to connect a debugger
-      Thread.sleep(pauseForDebugging);
-
       scan.run();
 
       return 0;
